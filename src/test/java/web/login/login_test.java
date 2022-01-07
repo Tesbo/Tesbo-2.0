5183c7a02e771cdaa675c21921d24f0ce16fe77d
viral patel <vir@qable.io>
2022-01-07 14:41:33 +0530

    Changed the directory format

--- src/test/java/web/login/login_test.java
+++ src/test/java/web/login/login_test.java
@@ -1,4 +1,4 @@
-package webapptest.login;
+package web.login;
 
 import object_repository.Web.login.login_page_step;
 import io.unity.image_comparison;
@@ -18,13 +18,13 @@ public class login_test extends base {
         login_page = new login_page_step(driver);
         compare =  new image_comparison(driver);
 
-
         login_page.verify_enter_your_email_text_box_is_present_on_page();
 
         compare.compare_image("homepage");
         login_page.enter_text_at_enter_your_email_text_box("ankitpatel@qable.io");
         login_page.enter_text_at_enter_your_password_password("QAble@2020");
         login_page.click_on_sign_in_button();
+        compare.compare_image("login_page");
 
     }
 
