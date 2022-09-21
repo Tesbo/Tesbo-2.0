c1121ab4bc33f9ca4c9f813b3b9c3f5ad6c269b7
viral patel <vir@qable.io>
2022-09-21 20:35:48 +0530

    Fixed The issue with cross confing for the platform

--- src/main/java/io/unity/performaction/automob/Device.java
+++ src/main/java/io/unity/performaction/automob/Device.java
@@ -3,13 +3,15 @@ package io.unity.performaction.automob;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import io.unity.framework.readers.json_file_reader;
+import io.unity.framework.runner.TestRunner;
 import io.unity.performaction.autoweb.Element;
 import io.unity.performaction.autoweb.locator_reader;
 
 public class Device {
     AppiumDriver driver;
     Element element;
-    locator_reader reader = new locator_reader();
+    json_file_reader reader = new json_file_reader();
 
     public Device(AppiumDriver dri) {
         this.driver = dri;
@@ -19,10 +21,10 @@ public class Device {
 
     public void switch_to_context(String context_name) {
 
-        if (reader.getPlatform().equalsIgnoreCase("android")) {
+        if (reader.getPlatform(TestRunner.currentConfig).equalsIgnoreCase("android")) {
             ((AndroidDriver) driver).context(context_name);
         }
-        if (reader.getPlatform().equalsIgnoreCase("iOS")) {
+        if (reader.getPlatform(TestRunner.currentConfig).equalsIgnoreCase("iOS")) {
             ((IOSDriver) driver).context(context_name);
         }
     }
