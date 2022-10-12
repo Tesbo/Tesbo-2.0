447a1d66c177c63ab4db49b3e7aaa8c7997fe305
ViralPatel <viral.patel@nevvon.com>
2022-10-12 17:38:18 +0530

    Updated Testname File

--- src/main/java/io/unity/framework/remotegrid/LambdaTestConfig.java
+++ src/main/java/io/unity/framework/remotegrid/LambdaTestConfig.java
@@ -1,7 +1,10 @@
 package io.unity.framework.remotegrid;
 
+import io.unity.framework.readers.json_file_reader;
+import io.unity.framework.runner.TestRunner;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.grid.config.JsonConfig;
 
 public class LambdaTestConfig {
 
@@ -15,8 +18,12 @@ public class LambdaTestConfig {
 
     public void addTestName(String TestName) {
 
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
-        executor.executeScript("lambda-name=" + TestName + "");
+        json_file_reader config = new json_file_reader();
+        if (config.get_grid_platForm(TestRunner.currentConfig).equalsIgnoreCase("lambdatest") || config.get_appium_platform(TestRunner.currentConfig).equalsIgnoreCase("lambdaTest")) {
+            JavascriptExecutor executor = (JavascriptExecutor) driver;
+            executor.executeScript("lambda-name=" + TestName + "");
+        }
+
     }
 
     public void markTestPassed() {
