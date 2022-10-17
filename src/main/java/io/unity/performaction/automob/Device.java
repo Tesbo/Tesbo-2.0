cb08753ce8e570dc2783d934973d525ddb962c0e
ViralPatel <viral.patel@nevvon.com>
2022-10-17 19:10:59 +0530

    add new report folder

--- src/main/java/io/unity/performaction/automob/Device.java
+++ src/main/java/io/unity/performaction/automob/Device.java
@@ -1,21 +1,24 @@
 package io.unity.performaction.automob;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.functions.ActionSupplier;
 import io.appium.java_client.ios.IOSDriver;
 import io.unity.framework.readers.json_file_reader;
 import io.unity.framework.runner.TestRunner;
 import io.unity.performaction.autoweb.Element;
-import io.unity.performaction.autoweb.locator_reader;
-import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 public class Device {
     AppiumDriver driver;
     Element element;
     json_file_reader reader = new json_file_reader();
-
+    private Duration STEP_DURATION = Duration.ofMillis(1);
+    private Duration NO_TIME = Duration.ofMillis(0);
+    private PointerInput.Origin VIEW = PointerInput.Origin.viewport();
     public Device(AppiumDriver dri) {
         this.driver = dri;
         element = new Element(driver);
@@ -33,8 +36,26 @@ public class Device {
     }
 
 
+    public void scroll_down() {
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+        int startY = (int) ((driver.manage().window().getSize().getHeight()) * 0.80);
+        int endY = (int) ((driver.manage().window().getSize().getHeight()) * 0.20);
+
+        int startX = (int) ((driver.manage().window().getSize().getWidth()) * 0.50);
+        Sequence circle = new Sequence(finger, 0);
+
+        circle.addAction(finger.createPointerMove(NO_TIME, VIEW, startX, startY));
+        circle.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
 
 
+        for (int i = 1; i < 300 + 1; i++) {
+            circle.addAction(finger.createPointerMove(STEP_DURATION, VIEW, startX, startY - (i+10)));
+        }
+
+        driver.perform(Arrays.asList(circle));
+
+    }
 
 
 }
